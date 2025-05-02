566bd88e530c1857460962075823b8bddb6663fa
Hans Erik Jepsen <hanserikjepsen@hotmail.com>
2025-05-02 09:50:43 +0200

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI4MDA1Mw/spectral.js?branch_slug=main&token=d3ec415d-209e-49a8-869c-f6f7c8aceafc";
+export default { extends: ruleset };
